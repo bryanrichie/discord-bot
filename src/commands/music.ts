08a57cc3401b5805beaa23f6f49ae7884c7220d9
RICHIEm8 <48461113+RICHIEm8@users.noreply.github.com>
2020-03-23 13:04:42 +1100

    rewrote play command

--- src/commands/music.ts
+++ src/commands/music.ts
@@ -7,36 +7,70 @@ export const skipCommand = `${process.env.PREFIX}skip`;
 export const stopCommand = `${process.env.PREFIX}stop`;
 export const queueCommand = `${process.env.PREFIX}queue`;
 
+function isUrl(value: string) {
+  const regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+  return regexp.test(value);
+}
+
 export async function handlePlayCommand(message: discord.Message) {
-  console.log("hello");
   const query = message.content.substring(playCommand.length).trim();
-  console.log(query);
-  const opts = {
-    maxResults: 5,
-    key: `${process.env.YOUTUBE_TOKEN}`,
-    type: "video"
-  };
-  const results = await youtubeSearch(query, opts);
-  console.log(results);
+  let songLink;
 
-  if (results) {
-    const youtubeResults = results.results;
-    const titles = youtubeResults.map((result, index) => {
-      return `${index + 1}) ${result.title}`;
-    });
+  if (isUrl(query)) {
+    songLink = query;
+    console.log(songLink);
+  } else {
+    const opts = {
+      maxResults: 5,
+      key: `${process.env.YOUTUBE_TOKEN}`,
+      type: "video"
+    };
+    const results = await youtubeSearch(query, opts);
 
-    message.channel.send({
-      embed: {
-        title: "Song selection. Type the song number to continue.",
-        description: titles.join("\n")
-      }
-    });
+    if (results) {
+      const youtubeResults = results.results;
+      const titles = youtubeResults.map((result, index) => {
+        return `${index + 1}) ${result.title}`;
+      });
 
-    const filter = (m: any) => m.author.id === message.author.id;
-    const collected = await message.channel.awaitMessages(filter, {
-      max: 1
-    });
+      message.channel.send({
+        embed: {
+          title: "Song selection. Type the song number to continue.",
+          description: titles.join("\n")
+        }
+      });
 
-    const selected = youtubeResults[parseInt(collected.first().content) - 1];
+      const filter = (m: any) => m.author.id === message.author.id;
+      const songChoice = await message.channel.awaitMessages(filter, {
+        max: 1
+      });
+
+      songLink = youtubeResults[parseInt(songChoice.first().content) - 1].link;
+    }
   }
+  if (!songLink) {
+    return await message.channel.send("Unable to find a song link!");
+  }
+
+  const voiceChannel = message.member.voiceChannel;
+
+  if (!voiceChannel)
+    return message.channel.send(
+      "You need to be in a voice channel to play music!"
+    );
+
+  const permissions = voiceChannel.permissionsFor(message.client.user);
+
+  if (
+    permissions &&
+    (!permissions.has("CONNECT") || !permissions.has("SPEAK"))
+  ) {
+    return message.channel.send(
+      "I need the permissions to join and speak in your voice channel!"
+    );
+  }
+  //  here we try to join the voice chat and save our connection into our object
+  const connection = await voiceChannel.join();
+
+  connection.playStream(ytdl(songLink));
 }
