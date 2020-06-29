aaaecc6eb797b240272a69a2b52adaba47564eae
RICHIEm8 <48461113+RICHIEm8@users.noreply.github.com>
2020-06-29 13:06:34 +1000

    feat(queue):added a queue function that allows queueing up songs and automatically playing songs within the queue and also removing finished songs from the queue

--- src/commands/music.ts
+++ src/commands/music.ts
@@ -1,11 +1,14 @@
 import * as discord from "discord.js";
 import * as youtubeSearch from "youtube-search";
 import * as ytdl from "ytdl-core";
+import * as _ from "lodash";
 
 export const joinCommand = `${process.env.PREFIX}join`;
 export const playCommand = `${process.env.PREFIX}play`;
 export const stopCommand = `${process.env.PREFIX}stop`;
 export const leaveCommand = `${process.env.PREFIX}leave`;
+
+let playQueue: string[] = [];
 
 //  JOIN COMMAND
 export async function handleJoinCommand(message: discord.Message) {
@@ -52,14 +55,37 @@ export async function handlePlayCommand(message: discord.Message) {
   }
   const info = await ytdl.getInfo(query);
   const connection = await message.member.voiceChannel.join();
-  const dispatcher = await connection.playStream(
-    ytdl(query, {
-      filter: "audioonly",
-      quality: "highestaudio",
-    })
-  );
-  dispatcher.setVolume(0.2);
-  return await message.channel.send(`**Now playing: ${info.title}**`);
+
+  playQueue.push(query);
+
+  if (playQueue.length > 1) {
+    return await message.channel.send(
+      `**${info.title} has been added to the queue!**`
+    );
+  }
+
+  const dispatch = () => {
+    const stream = connection.playStream(
+      ytdl(playQueue[0], {
+        filter: "audioonly",
+        quality: "highestaudio",
+      })
+    );
+
+    stream.on("end", () => {
+      playQueue = _.drop(playQueue, 1);
+      if (!_.isEmpty(playQueue)) {
+        //  called again if there are still songs to play
+        dispatch();
+      }
+    });
+
+    stream.setVolume(0.2);
+  };
+
+  dispatch();
+
+  await message.channel.send(`**Now playing: ${info.title}**`);
 }
 
 //  STOP COMMAND
@@ -79,11 +105,12 @@ export async function handleStopCommand(message: discord.Message) {
     );
   }
 
-  const dispatcher = await message.guild.voiceConnection.dispatcher;
+  const dispatcher = message.guild.voiceConnection.dispatcher;
 
   if (dispatcher) {
     dispatcher.end();
-    return await message.channel.send(`**Music has been stopped!**`);
+    await message.channel.send(`**Music has been stopped!**`);
+    console.log("music stopped", playQueue);
   } else {
     return await message.channel.send(
       `**I'm not currently playing any music!**`
